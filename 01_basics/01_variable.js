9a48b888e4658c10942138eafc697e2e712e7b0a
Reyansh13 <112423748+Reyansh13@users.noreply.github.com>
2024-04-01 11:33:51 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 14333
+let accountEmail ="reyansh992@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+console.log(accountId)
+// accountId = 2// not allowed
+/*
+prefer not to use var
+beacuse of issue in block scope and functional scope
+*/
+
+ accountEmail = "rc@gmail.com"
+ accountPassword = "21212"
+ accountCity = "delhi"
+
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
