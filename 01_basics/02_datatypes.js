5e112628278bfaa34d5004611ce032f040ab0d4e
Reyansh13 <112423748+Reyansh13@users.noreply.github.com>
2024-04-02 08:28:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //treat all jscode as  newer version
+// alert(3+3)
+
+
+//number 2 to power 53
+//bigint
+//string=" "
+//boolean = true/false
+//null = stand alone value
+//undefined
+//symbol = unique
+
+//object
+
+console.log(typeof age);
